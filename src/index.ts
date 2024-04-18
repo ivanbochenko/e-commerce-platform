61c57a89ebca612fce5c02d42bc62dc4bef63e21
ivan <bochenkoivan@gmail.com>
2024-04-18 13:29:57 +0300

    add db

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,22 @@
 import { Elysia } from "elysia";
+import {PrismaClient} from "@prisma/client";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const prisma = new PrismaClient();
+
+// create a new user
+// await prisma.user.create({
+//   data: {
+//     name: "Ivan Boch",
+//     email: `Ivan-${Math.random()}@example.com`,
+//   },
+// });
+
+// count the number of users
+const count = await prisma.user.count();
+
+const users = await prisma.user.findMany();
+
+const app = new Elysia().get("/", () => `There are ${count} users in the database.`).listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
