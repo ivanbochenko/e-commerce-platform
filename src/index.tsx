4227d6f90b1bc86dde2784668ec3962c007028d1
ivan <bochenkoivan@gmail.com>
2024-07-30 18:12:36 +0300

    htmx route

--- src/index.tsx
+++ src/index.tsx
@@ -17,26 +17,39 @@ const app = new Elysia()
     key.value = key.value ?? "new key"
     return (
       <Layout >
-        <div>
-          {/* <p>{key.value}</p> */}
-          {(await db.item.findMany()).map( item => 
-            <div>
-              <a href="/">{item.name}</a>
-            </div>
-          )}
+        <div id="replaceMe">
+          <p>{key.value}</p>
+          <button type="button" class="text-white bg-sky-700 hover:bg-sky-800 focus:ring-4 focus:ring-sky-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-sky-600 dark:hover:bg-sky-700 focus:outline-none dark:focus:ring-sky-800"
+            hx-get="/items"
+            hx-trigger="click"
+            hx-target="#replaceMe"
+            hx-swap="outerHTML"
+          >
+            Click Me!
+          </button>
         </div>
       </Layout>
+    )
+  })
+  .get("/items", async ({ cookie: { key } }) => {
+    const items = await db.item.findMany()
+    return (
+      <div>
+        {items.map( item => 
+          <div>
+            <a href="/">{item.name}</a>
+          </div>
+        )}
+      </div>
     )
   })
   .group('/user', app => app
     .get('/:email', async ({ params: { email } }) => {
       const user = await db.user.findUnique({where: { email }})
-      return <Layout>
-        <div>
+      return <div>
           <p>{user?.name}</p>
           <p>{user?.email}</p>
         </div>
-      </Layout>
     })
     .post('/sign-in', () => 'Sign in')
     .post('/sign-up', () => 'Sign up')
