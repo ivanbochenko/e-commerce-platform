2817b42271cfc81c44c47b9b4583d46e2cf846fb
ivan <bochenkoivan@gmail.com>
2024-11-08 12:36:54 +0200

    env db

--- src/db.ts
+++ src/db.ts
@@ -1,4 +1,4 @@
 import { Database } from "bun:sqlite";
 
-export const db = new Database("dev.db", { strict : true });
+export const db = new Database(process.env.DATABASE_URL, { strict : true });
 db.exec("PRAGMA journal_mode = WAL;");
